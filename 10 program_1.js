7c49fb94b76c23e32281c1c404b939f130a50e2c
Muhammad <faisal.wasim21@gmail.com>
2024-07-18 05:39:54 +0500

    Second Commit

--- 10 program_1.js
+++ 10 program_1.js
@@ -1,10 +1,6 @@
-
-
+"use strict";
 // program written by: faysal
 // Current Date: 15-04-2024
-
 // This program will write Hello World
-
 console.log('Hello World');
-
 console.log('taha');
---
+++ 10 program_2.js
@@ -0,0 +1,5 @@
+"use strict";
+// program written by: faysal
+// Current Date: 15-04-2024
+// This program will add two digits.
+console.log(2 + 2);
